3395d582646666ff2eb8020d964adcdbd72e0223
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-08-22 12:35:06 +0200

    two commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./header";
+import Technology from "./Technology";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Technology/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Technology.tsx
@@ -0,0 +1,13 @@
+const Technology = () => {
+    return (
+        <div>
+            simple HTML
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>)
+}
+export default Technology;
