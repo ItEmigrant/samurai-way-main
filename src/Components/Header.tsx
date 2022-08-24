62bcbcfa9f934074fe8efd26c71ba017037b09ce
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-08-24 15:25:08 +0200

    headercomponents

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>  <img src="https://th.bing.com/th/id/OIP.sxdELx88HQjlvgfXWUENawHaHa?w=173&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" />
+        </header>
+    );
+};
+
+export default Header;
