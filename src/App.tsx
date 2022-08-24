aa231db783b92090b58511fae26ad9aca2323811
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-08-24 15:16:45 +0200

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img
-                    src="https://th.bing.com/th/id/OIP.sxdELx88HQjlvgfXWUENawHaHa?w=173&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"/>
-            </header>
-            <nav className='nav'>
-                <div><a> Profile </a></div>
-                <div><a> Message </a></div>
-                <div><a> News </a></div>
-                <div><a> Music</a></div>
-                <div><a> Settings</a></div>
-            </nav>
-            <div className='content'>
-                <div><img
-                    src='https://th.bing.com/th/id/OIP.8xzxxZAXah8Uv7uhtR3e7QHaCs?w=312&h=127&c=7&r=0&o=5&dpr=1.5&pid=1.7'/>
-                </div>
-                <div>Ava description</div>
-                <div>My post
-                    <div>New post</div>
-                </div>
-
-                <div>
-                    <div> post1</div>
-                    <div>post2</div>
-                </div>
-
-            </div>
-        </div>
-    );
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>);
 }
 
 export default App;
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img
+                src='https://th.bing.com/th/id/OIP.8xzxxZAXah8Uv7uhtR3e7QHaCs?w=312&h=127&c=7&r=0&o=5&dpr=1.5&pid=1.7'/>
+            </div>
+            <div>Ava description</div>
+            <div>My post
+                <div>New post</div>
+            </div>
+
+            <div>
+                <div> post1</div>
+                <div>post2</div>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Profile;
