59dbcd8b4a8eb4601940f71a228204ee90a65285
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-09-01 15:14:32 +0200

    propsLesson15

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -3,18 +3,18 @@ import s from './MyPosts.module.css'
 import {Post} from "./Posts/Post";
 
 const MyPosts = () => {
-    return <div className={s.posts}>
-            My post
-            <div >
-                New post
-            </div>
-            <div >
-               <Post />
-               <Post />
-               <Post />
-
-            </div>
+    return <div>
+        My post
+        <div>
+            <textarea></textarea>
+            <button>Add post</button>
         </div>
+        <div className={s.posts}>
+            <Post message="Hi, how are you?" like="15 likes"/>
+            <Post message='My first post!' like="25 likes"/>
+
+        </div>
+    </div>
 };
 
 export default MyPosts;
--- src/Components/Profile/MyPosts/Posts/Post.tsx
+++ src/Components/Profile/MyPosts/Posts/Post.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-export const Post = () => {
-    return (
+type PostPropsType = {
+    message:string
+    like:string
+}
+export const Post = (props:PostPropsType) => {
 
+    return (
         <div className={s.item}>
-            post1
+            <img className={s.img} src='https://i.pinimg.com/736x/ed/c0/d8/edc0d87c0f024c849827d5b3d1c9bcfc--monkeys-warriors.jpg' alt={"samurai"}/>
+            {props.message}
+            <div>
+                <span>{props.like}</span>
+            </div>
         </div>
 
     );
