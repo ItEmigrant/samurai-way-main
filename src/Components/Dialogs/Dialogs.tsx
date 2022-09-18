35315f31119c23f7294eb13ce51da4fd6b145713
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-09-18 13:29:07 +0200

    'lessinDialogs21'

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-/*import s from './Dialogs.module.css'*/
+import s from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
-        <div >
-            DIALOGS
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.nameDialog}>
+                    Bogdan
+                </div>
+                <div className={s.nameDialog + ' ' + s.active}>
+                    Andrian
+                </div>
+                <div className={s.nameDialog}>
+                    Luda
+                </div>
+                <div className={s.nameDialog}>
+                    Artem
+                </div>
+                <div className={s.nameDialog}>
+                    Alisa
+                </div>
+            </div>
+            <div className= {s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>Hello Bro!</div>
+                <div className={s.message}> Have a nice day </div>
+            </div>
         </div>
     );
 };
