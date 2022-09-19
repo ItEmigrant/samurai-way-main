5e74e1571fdbf341f15976989a2db6ed2728078f
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-09-19 11:12:41 +0200

    RoutLesson22

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.nameDialog}>
-                    Bogdan
+                   <NavLink to='/dialogs/1'> Bogdan </NavLink>
                 </div>
                 <div className={s.nameDialog + ' ' + s.active}>
-                    Andrian
+                   <NavLink to='/dialogs/2'> Andrian </NavLink>
                 </div>
                 <div className={s.nameDialog}>
-                    Luda
+                    <NavLink to='/dialogs/3'> Luda </NavLink>
                 </div>
                 <div className={s.nameDialog}>
-                    Artem
+                    <NavLink to='/dialogs/4'> Artem </NavLink>
                 </div>
                 <div className={s.nameDialog}>
-                    Alisa
+                    <NavLink to='/dialogs/5'> Alisa </NavLink>
                 </div>
             </div>
             <div className= {s.messages}>
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return (
         <div>
-            <div><img className={s.image} src='https://th.bing.com/th/id/OIP.8xzxxZAXah8Uv7uhtR3e7QHaCs?w=312&h=127&c=7&r=0&o=5&dpr=1.5&pid=1.7' alt={'Profile'}/>
+            <div><img className={s.image} src='https://th.bing.com/th/id/R.954fb2083d4558551f76256530fa7e21?rik=6PFqxPlFR7jJHA&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2fsamurai%2fsamurai-07.jpg&ehk=%2fIDimYnMP5aCufQmVxdGJrTpLTwEE2mTMG32M8uzGLY%3d&risl=&pid=ImgRaw&r=0' alt={'Profile'}/>
             </div>
             <div className={s.item}>Ava + description</div>
             <MyPosts />
