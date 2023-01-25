110eb24d048f0ffb4292cf44016df38a38eb972b
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-01-25 19:42:10 +0100

    lesson41.2

--- src/Redux/DialogsReducer.tsx
+++ src/Redux/DialogsReducer.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import {ActionsTypes, dialogPageType, messagesType, } from "./state";
+import {ActionsTypes, dialogPageType, messagesType,} from "./state";
 
 const DialogsReducer = (state: dialogPageType, action: ActionsTypes) => {
 
-    if (action.type === "SEND-MESSAGE") {
-        const newSendMessage: messagesType = {
-            id: new Date().getTime(),
-            message: state.newMessagePostText
-        };
-        state.messages.push(newSendMessage)
-        state.newMessagePostText = "";
+    switch (action.type) {
+        case "SEND-MESSAGE":
+            const newSendMessage: messagesType = {
+                id: new Date().getTime(),
+                message: state.newMessagePostText
+            };
+            state.messages.push(newSendMessage)
+            state.newMessagePostText = "";
 
+            return state
 
-    } else if (action.type === "UPDATE-NEW-POST-MESSAGE-TEXT") {
-        state.newMessagePostText = action.messageDialogs;
+        case "UPDATE-NEW-POST-MESSAGE-TEXT":
+            state.newMessagePostText = action.messageDialogs;
+            return state
+
+        default:
+            return state
 
 
     }
 
 
-    return state
-
 };
 export default DialogsReducer;
+
+/*
+if (action.type === "SEND-MESSAGE") {
+    const newSendMessage: messagesType = {
+        id: new Date().getTime(),
+        message: state.newMessagePostText
+    };
+    state.messages.push(newSendMessage)
+    state.newMessagePostText = "";
+
+
+} else if (action.type === "UPDATE-NEW-POST-MESSAGE-TEXT") {
+    state.newMessagePostText = action.messageDialogs;
+
+
+}
+
+
+return state
+*/
 
--- src/Redux/ProfileReducer.tsx
+++ src/Redux/ProfileReducer.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import {ActionsTypes, postsType, profilePageType} from "./state";
 
+
 const ProfileReducer = (state: profilePageType, action: ActionsTypes) => {
 
+    switch (action.type) {
+        case "ADD-STATE-POST-MESSAGE":
+            const newPost: postsType = {
+                id: new Date().getTime(),
+                message: state.messageForNewPosts,
+                likeCount: 1
+            }
+            state.posts.push(newPost);
+            state.messageForNewPosts = "";
 
-    if (action.type === "ADD-STATE-POST-MESSAGE") {
+            return state;
 
-        const newPost: postsType = {
-            id: new Date().getTime(),
-            message: state.messageForNewPosts,
-            likeCount: 1
-        };
-        state.posts.push(newPost);
-        state.messageForNewPosts = "";
+        case "UPDATE-NEW-POST-TEXT":
+            state.messageForNewPosts = action.postMessage;
+            return state;
 
+        default:
+            return state;
 
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
-        state.messageForNewPosts = action.postMessage;
 
     }
+}
 
-    return state;
-
-};
 
 export default ProfileReducer;
+/*if (action.type === "ADD-STATE-POST-MESSAGE") {
+            const newPost: postsType = {
+        id: new Date().getTime(),
+        message: state.messageForNewPosts,
+        likeCount: 1
+    };
+    state.posts.push(newPost);
+    state.messageForNewPosts = "";
+
+
+
+} else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    state.messageForNewPosts = action.postMessage;
+
+}
+
+return state;
+
+};*/
+
