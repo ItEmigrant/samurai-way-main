abd548083a3515c3c63429839e907b8007c5222d
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-06-30 21:49:43 +0200

    lesson71lessonEnd

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -18,14 +18,13 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
                       src='https://th.bing.com/th/id/R.954fb2083d4558551f76256530fa7e21?rik=6PFqxPlFR7jJHA&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2fsamurai%2fsamurai-07.jpg&ehk=%2fIDimYnMP5aCufQmVxdGJrTpLTwEE2mTMG32M8uzGLY%3d&risl=&pid=ImgRaw&r=0'
                       alt={'Profile'}/>
             </div>
-            <ProfileStatus/>
+            <ProfileStatus status={'I am samurai'}/>
             <div className={s.item}>
                 <img src={props.profile.photos.large} alt={"avatar"}/>
                 <div>{props.profile.fullName}</div>
                 <div>{props.profile.aboutMe}</div>
                 <div>{props.profile.contacts.instagram}</div>
                 <div>{props.profile.lookingForAJobDescription}</div>
-
             </div>
         </div>
     )
--- src/Components/Profile/ProfileStatus.tsx
+++ src/Components/Profile/ProfileStatus.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 
-export const ProfileStatus = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+type ProfileStatusPropsType = {
+    status: string
+}
+
+type stateProfileStatusType = {
+    editMode: boolean
+}
+
+export class ProfileStatus extends React.Component <ProfileStatusPropsType, stateProfileStatusType> {
+
+    state: stateProfileStatusType = {
+        editMode: false
+
+    }
+
+    activateEditeMode() {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deActivateEditeMode() {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                {!this.state.editMode && <div>
+                    <span onDoubleClick={this.activateEditeMode.bind(this)}>{this.props.status}</span>
+                </div>}
+
+                {this.state.editMode && <div>
+                    <input autoFocus onBlur={this.deActivateEditeMode.bind(this)} value={this.props.status}/>
+                </div>}
+
+            </div>
+        );
+    }
+}
+
+
 
