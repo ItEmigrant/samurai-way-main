3420ddbf9462dd947ebc3df4fb36bcc30051f948
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-07-04 09:33:42 +0200

    lesson75start

--- src/Components/LOGIN/Login.tsx
+++ src/Components/LOGIN/Login.tsx
@@ -2,7 +2,23 @@ import React from 'react';
 
 export const Login = () => {
     return (
-       <h1>Login</h1>
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <input placeholder={'Login'}/>
+                </div>
+                <div>
+                    <input placeholder={'Password'}/>
+                </div>
+                <div>
+                    <input type={"checkbox"}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </div>
     );
 };
 
