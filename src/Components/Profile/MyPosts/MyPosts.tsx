7f1d1636ec5e55cc0ebdfd05683c54465cb6dc0d
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-09-22 09:15:29 +0200

    lesson87reactMemoEnd

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -8,25 +8,19 @@ type ValuesPostType = {
     addNewPostBody: string;
 }
 
-class MyPosts extends React.PureComponent<ProfileCommonType> {
-    /* shouldComponentUpdate(nextProps: Readonly<ProfileCommonType>, nextState: Readonly<{}>): boolean {
-       return nextProps !== this.props || nextState !== this.state
-   }*/
-
-    render() {
-        const addNewPost = (values: ValuesPostType) => {
-            this.props.addPosts(values.addNewPostBody);
-        }
-        return <div className={s.MyPostsBlock}>
-            <h3>My post</h3>
-            <div>
-                <ReduxAddPostForm onSubmit={addNewPost}/>
-            </div>
-            <div className={s.posts}>
-                {this.props.posts.map(el => (<Post key={el.id} message={el.message} likeCount={el.likeCount}/>))}
-            </div>
-        </div>
+const MyPosts = React.memo((props: ProfileCommonType) => {
+    const addNewPost = (values: ValuesPostType) => {
+        props.addPosts(values.addNewPostBody);
     }
-}
+    return <div className={s.MyPostsBlock}>
+        <h3>My post</h3>
+        <div>
+            <ReduxAddPostForm onSubmit={addNewPost}/>
+        </div>
+        <div className={s.posts}>
+            {props.posts.map(el => (<Post key={el.id} message={el.message} likeCount={el.likeCount}/>))}
+        </div>
+    </div>
+})
 
 export default MyPosts;
